09d00e784b347fa83b80fb8ed751107e0a502f19
OZIIJIN <dpwlsdlqma6@gmail.com>
2024-11-29 13:47:13 +0900

    [feat] create controller/service/repo

---
+++ src/main/java/org/project/stockqueen/controller/DemandController.java
@@ -0,0 +1,13 @@
+package org.project.stockqueen.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/demands")
+public class DemandController {
+
+
+}
