7c8f82e865b8e976b46614f485ea8fd45139aeab
OZIIJIN <dpwlsdlqma6@gmail.com>
2024-12-08 00:32:12 +0900

    파이썬 서버 연결 해제

--- src/main/java/org/project/stockqueen/repository/DemandJpaRepository.java
+++ src/main/java/org/project/stockqueen/repository/DemandJpaRepository.java
@@ -1,8 +1,11 @@
 package org.project.stockqueen.repository;
 
 import org.project.stockqueen.entity.DemandForecasting;
+import org.project.stockqueen.entity.Menu;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface DemandJpaRepository extends JpaRepository<DemandForecasting, Long> {
+
+  DemandForecasting findByMenu(Menu menu);
 
 }
--- src/main/java/org/project/stockqueen/service/MenuService.java
+++ src/main/java/org/project/stockqueen/service/MenuService.java
@@ -4,15 +4,12 @@ import java.util.ArrayList;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.project.stockqueen.dto.DemandForecastingResponse;
-import org.project.stockqueen.dto.LstmResponse;
 import org.project.stockqueen.dto.MenuNameResponseDto;
 import org.project.stockqueen.entity.Menu;
 import org.project.stockqueen.repository.DemandJpaRepository;
 import org.project.stockqueen.repository.MenuJpaRepository;
 import org.project.stockqueen.repository.sales_history.SalesHistoryJpaRepository;
 import org.project.stockqueen.repository.sales_history.SalesHistoryQuery;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -32,20 +29,20 @@ public class MenuService {
         .orElseThrow(() -> new IllegalArgumentException("해당 메뉴는 존재하지 않습니다."));
 
     //flask로 수요 예측 결과를 받아와야함
-    String url = "https://5808-122-36-149-213.ngrok-free.app/menu-predictions";
-
-    HttpHeaders headers = new HttpHeaders();
-    headers.set("Content-Type", "application/json");
-
-    String requestBody = String.format("{\"menu_id\": %d}", menu.getId());
-
-    HttpEntity<String> requestEntity = new HttpEntity<>(requestBody, headers);
-
-    LstmResponse response = restTemplate.postForObject(
-        url,
-        requestEntity,
-        LstmResponse.class
-    );
+//    String url = "http://localhost:5000/menu-predictions";
+//
+//    HttpHeaders headers = new HttpHeaders();
+//    headers.set("Content-Type", "application/json");
+//
+//    String requestBody = String.format("{\"menu_id\": %d}", menu.getId());
+//
+//    HttpEntity<String> requestEntity = new HttpEntity<>(requestBody, headers);
+//
+//    LstmResponse response = restTemplate.postForObject(
+//        url,
+//        requestEntity,
+//        LstmResponse.class
+//    );
 
     //받아온 결과를 db에 저장하고
 
@@ -53,8 +50,10 @@ public class MenuService {
 
     int previousSum = salesHistoryQuery.sumSalesByMonth(menu.getId());
 
-    long predictedSum = Math.round(
-        response.getPrediction().stream().mapToDouble(Double::doubleValue).sum());
+//    long predictedSum = Math.round(
+//        response.getPrediction().stream().mapToDouble(Double::doubleValue).sum());
+
+    int predictedSum = demandJpaRepository.findByMenu(menu).getPredictedValue();
 
     return new DemandForecastingResponse(
         11, previousSum, 12, (int) predictedSum, menu.getMenuName()
